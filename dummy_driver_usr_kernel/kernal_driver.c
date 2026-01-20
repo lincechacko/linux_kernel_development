8cd480ddcbe2b14f7dc62bc4f9bf2a7651258853
lince <lincechacko99@gmail.com>
2026-01-20 20:50:23 +0530

    added the kernel init function

--- dummy_driver_usr_kernel/kernal_driver.c
+++ dummy_driver_usr_kernel/kernal_driver.c
@@ -9,6 +9,8 @@
 #include<linux/slab.h>
 #include<linux/uaccess.h>
 
+#define MEMORY_SIZE 1024
+
 dev_t device;
 static struct class * device_class;
 static struct cdev device_cdev;
@@ -30,6 +32,7 @@ static struct file_operations fops =
 };
 
 
+
 static int __init init_function(void)
 {
 	/*allocate the major and minor number*/
@@ -39,18 +42,46 @@ static int __init init_function(void)
 		pr_err("allocation of major and minor numbeer has been failed\n");
 		return(-1);
 	}
-	pr_info("allocated major and minor number is %d %d",MAJOR(dev),MINOR(dev));
+	pr_info("allocated major and minor number is %d %d\n",MAJOR(dev),MINOR(dev));
 
-	/*cdev initialization*/
+	/*cdev initialization creating the cdev structure*/
 	cdev_init(&device_cdev , &fops);
 
+	/*adding the character device into the system */ 
 	if(cdev_add(&device_cdev,device,1) < 0)
 	{
 		unregister_chrdev_region(device, 1);
 		pr_err("char device adding is failed\n");
+		return(-1);
+	}
+	pr_info("device added successfully\n");
+
+	/*creating the struct class*/
+	if(IS_ERROR(device_class = create_class("device_class")))
+	{
+		unregister_chrdev_region(device);
+		pr_err("struct class creation failed\n");
+		return(-1);
+	}
+	/*creating the device*/
+	if(IS_ERROR(device_create(device_class,NULL ,dev, NULL,"new_device")))
+	{
+		class_destroy(device_class);
+		unregister_chrdev_region(device);
+		pr_err("device creation failed\n");
+		return(-1);
+	}
+	/*the device creation is success*/
+	if(kernel_buffer =  kmalloc(MEMORY_SIZE ,  GFP_KERNEL)==0)
+	{
+		class_destroy(device_class);
+		unregister_chrdev_region(device);
+		pr_err("memory allocation failed\n");
+		return(-1);
+
 	}
 
-
+	return(0);
 
 }
 
